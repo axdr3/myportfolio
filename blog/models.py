114c366673ef6e5d5167894ddc8f43e5e332f451
Axdre <axdber@gmail.com>
2019-12-17 22:44:16 -0300

    Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    image = models.ImageField(upload_to="images/")
+    title = models.CharField(max_length=20)
+    text = models.TextField(max_length=800)
+    pub_date = models.DateTimeField()
