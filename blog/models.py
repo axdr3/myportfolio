6dd752c16c01f61d8657d9774b8c095f3b9dc9c4
Axdre <axdber@gmail.com>
2019-12-20 19:04:58 -0300

    blogs 1.0

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,14 @@ from django.db import models
 class Blog(models.Model):
     image = models.ImageField(upload_to="images/")
     title = models.CharField(max_length=20)
-    text = models.TextField(max_length=800)
+    text = models.TextField()
     pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.text[:100] + '...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
